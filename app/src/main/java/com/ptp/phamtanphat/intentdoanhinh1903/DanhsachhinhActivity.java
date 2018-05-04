87c4a8ec202dfc418939fd69915c31c7a745242f
thuviengithub0208 <khoaphp@yahoo.com>
2018-05-04 21:32:54 +0700

    "Update"

--- app/src/main/java/com/ptp/phamtanphat/intentdoanhinh1903/DanhsachhinhActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/intentdoanhinh1903/DanhsachhinhActivity.java
@@ -4,20 +4,44 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 import java.util.ArrayList;
 
 public class DanhsachhinhActivity extends AppCompatActivity {
 
     ArrayList<String> manghinh;
+    TableLayout tableLayout;
+    int id = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_danhsachhinh);
 
+        tableLayout = findViewById(R.id.tablelayout);
         Intent intent = getIntent();
         manghinh = intent.getStringArrayListExtra("Manghinh");
 
+        int sodong = 6;
+        int socot = 3;
 
+        for (int i = 1 ; i <= sodong ; i++){
+            TableRow tableRow = new TableRow(this);
+            for (int j = 1 ; j <= socot ; j++){
+                ImageView imageView = new ImageView(this);
+                //socot * (i - 1) + j -1
+                int vitri = id++;
+                TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(190,190);
+                int idhinh = getResources().getIdentifier(manghinh.get(vitri),"drawable",getPackageName());
+                imageView.setImageResource(idhinh);
+                imageView.setLayoutParams(layoutParams);
+                tableRow.addView(imageView);
+            }
+            tableLayout.addView(tableRow);
+        }
     }
+
+
 }
